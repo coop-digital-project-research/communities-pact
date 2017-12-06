28fe25bca8855237472e853f0af50c361815ad61
James Almond <james@jamesalmond.com>
2017-12-06 15:45:54 +0000

    Password protect admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,7 @@
 class AdminController < ApplicationController
+
+  http_basic_authenticate_with name: "admin", password: Rails.application.secrets.admin_password
+
   def show
     @pact_members = PactMember.order('created_at ASC')
   end
