10eb87431df2b6d63c764f17765609276aa5108a
James Almond <james@jamesalmond.com>
2017-12-06 11:35:33 +0000

    Make the pact copy work better with plurals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,12 @@ module ApplicationHelper
     pact_text = "I, #{pact_member.name}, will #{pact.action}"
     if pact.member_difference > 0
       if pact.member_count == 1
-        pact_text << " and I need #{pluralize(pact.people_requirement, 'people')} to join me."
+        pact_text << " and I need #{pluralize(pact.people_requirement, 'more person')} to join me."
       else
-        pact_text << " with #{pact.member_names(without: pact_member)} and we need #{pluralize(pact.member_difference, 'people')} to join us."
+        pact_text << " with #{pact.member_names(without: pact_member)} and we need #{pluralize(pact.member_difference, 'more person')} to join us."
       end
+    else
+      pact_text << " with #{pact.member_names(without: pact_member)}."
     end
     pact_text
   end
