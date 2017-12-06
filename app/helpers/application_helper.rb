3b4eee360e5b1f27b2ad5eb11526b709342e8f3a
James Rice <james.rice@coopdigital.co.uk>
2017-12-06 11:08:25 +0000

    Abstract Pact partial content into a Helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def pact_text(pact_member, pact)
+    pact_text = "I, #{pact_member.name}, will #{pact.action}"
+    if pact.member_difference > 0
+      if pact.member_count == 1
+        pact_text << " and I need #{pluralize(pact.people_requirement, 'people')} to join me."
+      else
+        pact_text << " with #{pact.member_names(without: pact_member)} and we need #{pluralize(pact.member_difference, 'people')} to join us."
+      end
+    end
+    pact_text
+  end
 end
